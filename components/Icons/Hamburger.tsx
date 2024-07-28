dc1f1f3fb1da29ce7731d65d18d7b80ccf1fdc36
Ian Drilon <ian.drilon@mashupgarage.com>
2024-07-28 21:58:53 +0800

    updated behavior

--- components/Icons/Hamburger.tsx
+++ components/Icons/Hamburger.tsx
@@ -1,12 +1,16 @@
 export const HamburgerIcon = (): JSX.Element => (
   <svg
-    width='16'
-    height='8'
-    viewBox='0 0 16 8'
-    fill='none'
     xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 24 24'
+    strokeWidth={1.5}
+    stroke='currentColor'
+    className='w-6 h-6'
   >
-    <rect width='16' height='2' rx='1' fill='#350840' />
-    <rect y='6' width='16' height='2' rx='1' fill='#350840' />
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      d='M3.75 9h16.5m-16.5 6.75h16.5'
+    />
   </svg>
 )
